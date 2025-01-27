62f25b1cc2c12a48fca0c04c967b4c780c2d0179
kutluaylav <kutluay3344@hotmail.com>
2025-01-27 19:24:40 +0300

    producerkafkatopisc

---
+++ producer/src/main/java/com/kutluayulutas/producer/config/WikimediaTopicConfig.java
@@ -0,0 +1,15 @@
+package com.kutluayulutas.producer.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class WikimediaTopicConfig {
+
+    public NewTopic wikiMediaTopic() {
+        return TopicBuilder
+                .name("wikimedia-stream")
+                .build();
+    }
+}
